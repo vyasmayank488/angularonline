53779935ca52a753f8268e39c1afe8eb96d6575b
vyasmayank488 <vyasmayank488@gmail.com>
2020-06-12 15:15:51 +0530

    fh

--- angularonline/src/app/app.component.ts
+++ angularonline/src/app/app.component.ts
@@ -5,17 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   // template: `<mark>internal html</mark>`, // view : UI
-  // styles: [
-  //   `
-  //     li {
-  //       color: red;
-  //     }
-  //   `,
-  // ],
+  styles: [
+    `
+      .my-class {
+        color: red;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   // data and logic
   title = 'Angular Welcomes you..!';
+
+  abc = 'some text...!';
 
   arr = ['Pune', 'Hyderabad', 'Mumbai'];
   // console.log(arr);
@@ -26,18 +28,30 @@ export class AppComponent {
       name: 'Screen 5.5',
       description: '5.5 Screen for Moto g',
       inStock: 5,
+      prodColor: 'red',
+      canPurch: true,
+      clsVar: true,
+      country: 'Australia',
     },
     {
       id: 1002,
       name: 'Screen 5',
       description: '5 Screen for Samsung',
       inStock: 8,
+      prodColor: 'green',
+      canPurch: false,
+      clsVar: true,
+      country: 'India',
     },
     {
       id: 1003,
       name: 'key panel',
       description: 'Key panel for Nokia',
       inStock: 0,
+      prodColor: 'blue',
+      canPurch: true,
+      clsVar: true,
+      country: 'Germany',
     },
   ];
 }
